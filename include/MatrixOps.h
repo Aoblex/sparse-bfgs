a5056711a94e4663620e0ffe35ee3314d9ef41b3
Chenrui <2021111664@stu.sufe.edu.cn>
2024-12-12 23:12:19 +0800

    remove useless function

--- include/MatrixOps.h
+++ include/MatrixOps.h
@@ -2,8 +2,5 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
-// Sparsify a matrix by keeping only the top 10% of the elements
-Eigen::SparseMatrix<double> sparsifyMatrix(const Eigen::MatrixXd& matrix);
-
 // Generate a random discrete distribution of size N
 Eigen::VectorXd generateDiscreteDistribution(int N);
