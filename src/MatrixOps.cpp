30a73e9706084d5968620df9246752331648a066
Chenrui <2021111664@stu.sufe.edu.cn>
2024-12-12 23:13:49 +0800

    remove useless function

--- src/MatrixOps.cpp
+++ src/MatrixOps.cpp
@@ -5,34 +5,9 @@
 #include <Eigen/Sparse>
 #include <random>
 
-
-Eigen::SparseMatrix<double> sparsifyMatrix(const Eigen::MatrixXd& matrix) {
-    std::vector<double> elements;
-    for (int i = 0; i < matrix.rows(); ++i) {
-        for (int j = 0; j < matrix.cols(); ++j) {
-            elements.push_back(matrix(i, j));
-        }
-    }
-
-    std::nth_element(elements.begin(), elements.begin() + elements.size() * 0.1, elements.end());
-    double threshold = elements[elements.size() * 0.1];
-
-    Eigen::SparseMatrix<double> sparseMatrix(matrix.rows(), matrix.cols());
-    for (int i = 0; i < matrix.rows(); ++i) {
-        for (int j = 0; j < matrix.cols(); ++j) {
-            if (matrix(i, j) >= threshold) {
-                sparseMatrix.insert(i, j) = matrix(i, j);
-            }
-        }
-    }
-    sparseMatrix.makeCompressed();
-    return sparseMatrix;
-}
-
 Eigen::VectorXd generateDiscreteDistribution(int N) {
-    // Random number generator
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    // Random number generator with fixed seed
+    std::mt19937 gen(42); // Fixed seed for reproducibility
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
     // Generate N random numbers
